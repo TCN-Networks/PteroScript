f882acc01cca759fa33670d83343017ade3210b2
CursedGeneral <redplayz6969@gmail.com>
2024-07-11 11:32:17 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo -e "\e[0m"
+echo -e "\e[0m"
+echo -e "\e[0;95m████████╗ ██████╗███╗   ██╗    ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗███████╗"
+echo -e "\e[0;95m╚══██╔══╝██╔════╝████╗  ██║    ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔════╝"
+echo -e "\e[0;95m   ██║   ██║     ██╔██╗ ██║    ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ ███████╗"
+echo -e "\e[0;95m   ██║   ██║     ██║╚██╗██║    ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ╚════██║"
+echo -e "\e[0;95m   ██║   ╚██████╗██║ ╚████║    ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗███████║"
+echo -e "\e[0;95m   ╚═╝    ╚═════╝╚═╝  ╚═══╝    ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝"
+echo -e "\e[0m"
+echo -e "\e[0m"
+sleep 1
+echo -e "\e[101mYour container in 3 seconds...\e[0m"
+sleep 1
+echo -e "\e[101mYour container in 2 seconds...\e[0m"
+sleep 1
+echo -e "\e[101mYour container in 1 seconds...\e[0m"
+sleep 1
+echo -e "\e[101mProvisioning your container!\e[0m"
+echo -e "\e[0m"
+
+# Adding a 2-second delay
+sleep 3
+
+# clear your browsing history
+clear
+echo -e "\e[0m"
+echo -e "\e[0m"
+echo -e "\e[0;95m████████╗ ██████╗███╗   ██╗    ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗███████╗"
+echo -e "\e[0;95m╚══██╔══╝██╔════╝████╗  ██║    ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔════╝"
+echo -e "\e[0;95m   ██║   ██║     ██╔██╗ ██║    ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ ███████╗"
+echo -e "\e[0;95m   ██║   ██║     ██║╚██╗██║    ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ╚════██║"
+echo -e "\e[0;95m   ██║   ╚██████╗██║ ╚████║    ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗███████║"
+echo -e "\e[0;95m   ╚═╝    ╚═════╝╚═╝  ╚═══╝    ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝"
+echo -e "\e[0m"
+echo -e "\e[0m"
+
+echo ""
+echo -e "\e[32m#####################################################"
+echo -e "\e[32m"
+echo -e "\e[1;96m    Welcome to Your Server!"
+echo -e "\e[32m"
+echo -e "\e[1;96m    Here are some instructions and server details:"
+echo -e "\e[32m"
+echo -e "\e[92m    Hostname: $(hostname)"
+echo -e "\e[92m    IP address: $(curl -s ifconfig.me)"
+echo -e "\e[92m    Local IP address: $(hostname -I)"
+echo -e "\e[32m"
+echo -e "\e[93m    Please read and abide by the following:"
+echo -e "\e[93m    - Do not run XMRig or any other cryptominers without permissions."
+echo -e "\e[93m    - Do not run Minecraft servers or use this container for illegal purposes."
+echo -e "\e[32m"
+echo -e "\e[91m    By using this server, you agree to our Terms of Service and Privacy Policy:"
+echo -e "\e[91m    Terms of Service: https://tcnetwk.cloud/terms-of-service.html"
+echo -e "\e[91m    Privacy Policy: https://tcnetwk.cloud/privacy-policy.html"
+echo -e "\e[32m"
+echo -e "\e[32m#####################################################"
+echo -e "\e[0m"
+echo -e "\e[0m"
+
+# End of script
